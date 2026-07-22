e2a5c48616d7375c742256546a411a82f5cd21bc
booty butt hole <gd.suti844@gmail.com>
2026-07-22 05:00:45 +0200

    Change user-agent string in websitebotex.py
    Updated user-agent string for website bot.

--- .github/workflows/websitebotex.py
+++ .github/workflows/websitebotex.py
@@ -53,4 +53,7 @@ options.add_argument("--disable-dev-shm-usage")
 #options.add_argument(f"--user-data-dir={temp_dir}")
 
 # Bypass detection
-options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)
+options.add_argument(
+    "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+)
